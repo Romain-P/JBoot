e705da01a66ff0223e023e441d7347326e2d1480
Romain-p <romain.pillot@epitech.eu>
2017-10-23 13:59:08 +0200

    setted gradle and spring boot

---
+++ README.md
@@ -0,0 +1,5 @@
+## Jboot
+
+A simple web application with an embedded Jetty server.  
+ * Developped with `Intellij`  
+ * Run with `./gradlew build && java -jar build/libs/gs-spring-boot-0.1.0.jar`
---
+++ src/main/java/org/tetra/jboot/core/Application.java
@@ -0,0 +1,17 @@
+package org.tetra.jboot.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author: romain.pillot
+ * @Date: 23/10/2017
+ */
+
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
